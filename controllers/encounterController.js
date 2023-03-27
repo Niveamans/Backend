3e1e166bea58849cc13e0156a4939537338a8970
S.Ashwin <ashwinsriram11@gmail.com>
2023-03-27 21:28:07 +0530

    encounter controller added.

--- controllers/encounterController.js
+++ controllers/encounterController.js
@@ -7,28 +7,35 @@ const healthcare = google.healthcare({
   headers: { "Content-Type": "application/fhir+json" },
 });
 
-const parent = `projects/ehealth-record-01/locations/asia-south1/datasets/eHealthRecordDataset/fhirStores/myFhirStore/fhir/Patient`;
+const parent = `projects/ehealth-record-01/locations/asia-south1/datasets/eHealthRecordDataset/fhirStores/myFhirStore/fhir/Encounter`;
 const ogParent = `projects/ehealth-record-01/locations/asia-south1/datasets/eHealthRecordDataset/fhirStores/myFhirStore`;
 
-export const createPatientResource = async (req, res) => {
-  const { name, gender, birthDate, resourceType } = req.body;
-  // create.
-  const body = {
-    name: [
-      { use: `${name.use}`, family: `${name.family}`, given: `${name.given}` },
-    ],
-    gender: gender,
-    birthDate: birthDate,
+export const createEncounterResource = async (req, res) => {
+  const { status, subject, period, resourceType } = req.body;
+  const encounter = {
     resourceType: resourceType,
+    status: status,
+    subject: {
+      reference: `${subject.reference}`,
+    },
+    period: {
+      start: `${period.start}`,
+      end: `${period.end}`,
+    },
   };
 
-  const request = { ogParent, type: resourceType, requestBody: body };
+  const request = {
+    ogParent,
+    type: resourceType,
+    requestBody: encounter,
+  };
+
   const resource = await healthcare.projects.locations.datasets.fhirStores.fhir
     .create(request)
     .then((v) => {
       console.log(`Created patient resource with ID ${v.data.id}`);
       console.log(v.data);
-      res.status(200).send(v);
+      res.status(200).send(v.data);
     })
     .catch((e) => {
       console.log(e);
@@ -38,18 +45,18 @@ export const createPatientResource = async (req, res) => {
     });
 };
 
-export const updatePatientResource = async (req, res) => {
+export const updateEncounterResource = async (req, res) => {
   const resourceId = req.body.resourceId;
   const name = parent.concat("/", resourceId);
 
-  const body = { resourceType: "Patient", id: resourceId, active: true };
+  const body = { resourceType: "Encounter", id: resourceId, active: true };
   const request = { name, requestBody: body };
 
   const resource = await healthcare.projects.locations.datasets.fhirStores.fhir
     .update(request)
     .then((v) => {
       console.log(`Updated ${resourceType} resource:\n`, v.data);
-      res.status(204).send(v);
+      res.status(200).send(v.data);
     })
     .catch((e) => {
       console.log(e);
@@ -59,7 +66,7 @@ export const updatePatientResource = async (req, res) => {
     });
 };
 
-export const patchPatientResource = async (req, res) => {
+export const patchEncounterResource = async (req, res) => {
   const resourceId = req.body.resourceId;
   const patchOptions = JSON.parse(req.body.patchOptions);
   const name = parent.concat("/", resourceId);
@@ -72,7 +79,7 @@ export const patchPatientResource = async (req, res) => {
     .patch(request)
     .then((v) => {
       console.log(`Patched ${resourceType} resource`);
-      res.status(204).send(v.data);
+      res.status(200).send(v.data);
     })
     .catch((e) => {
       console.log(e);
@@ -82,7 +89,7 @@ export const patchPatientResource = async (req, res) => {
     });
 };
 
-export const getPatientResource = async (req, res) => {
+export const getEncounterResource = async (req, res) => {
   const resourceId = req.body.resourceId;
   const name = parent.concat("/", resourceId);
   const request = { name };
@@ -91,52 +98,25 @@ export const getPatientResource = async (req, res) => {
     .read(request)
     .then((v) => {
       console.log(`Got ${resourceType} resource:\n`, v.data);
-      res.status(200).send(v);
+      req.status(200).send(v.data);
     })
     .catch((e) => {
       console.log(e);
-      res.send(500).send({
+      req.status(500).send({
         message: "unknown error",
       });
     });
 };
 
-export const getPatientEverything = async (req, res) => {
-  const patientId = req.body.patientId;
-  const name = parent.concat("/", patientId);
-  const request = { name };
-
-  const patientEverything =
-    await healthcare.projects.locations.datasets.fhirStores.fhir
-      .PatientEverything(request)
-      .then((v) => {
-        console.log(
-          `Got all resources in patient ${patientId} compartment:\n`,
-          JSON.stringify(patientEverything)
-        );
-        res.status(200).send(v);
-      })
-      .catch((e) => {
-        console.log(e);
-        res.send(500).send({
-          message: "unknown error",
-        });
-      });
-};
-
-export const deletePatientResource = async (req, res) => {
+export const deleteEncounterResource = async (req, res) => {
   const resourceId = req.body.resourceId;
   const name = parent.concat("/", resourceId);
   const request = { name };
 
-  // Regardless of whether the operation succeeds or
-  // fails, the server returns a 200 OK HTTP status code. To check that the
-  // resource was successfully deleted, search for or get the resource and
-  // see if it exists.
-  await healthcare.projects.locations.datasets.fhirStores.fhir
+  const resource = await healthcare.projects.locations.datasets.fhirStores.fhir
     .delete(request)
     .then((v) => {
-      console.log("Deleted FHIR resource");
+      console.log("deleted FHIR resources.");
       res.status(200).send(v.data);
     })
     .catch((e) => {
