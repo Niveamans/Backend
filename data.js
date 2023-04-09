4d2967a90a8e2a61f7c18f5f88187291263bbd41
S.Ashwin <ashwinsriram11@gmail.com>
2023-04-09 14:33:10 +0530

    changed encounter controller

--- data.js
+++ data.js
@@ -1,58 +1,116 @@
-export const observationBody ={
-    "resourceType": "Observation",
-    "status": "final",
-    "subject": {
-      "reference": "Patient/a364e9f8-dd8b-460a-900b-5b905f0a0d3d"
+export const observationBody = {
+  resourceType: "Observation",
+  status: "final",
+  subject: {
+    reference: "Patient/a364e9f8-dd8b-460a-900b-5b905f0a0d3d",
+  },
+  effectiveDateTime: "2020-01-01T00:00:00+00:00",
+  identifier: [
+    {
+      system: "my-code-system",
+      value: "ABC-12345",
     },
-    "effectiveDateTime": "2020-01-01T00:00:00+00:00",
-    "identifier": [
+  ],
+  code: {
+    coding: [
       {
-        "system": "my-code-system",
-        "value": "ABC-12345"
-      }
+        system: "http://loinc.org",
+        code: "8867-4",
+        display: "Heart rate",
+      },
     ],
-    "code": {
-      "coding": [
-        {
-          "system": "http://loinc.org",
-          "code": "8867-4",
-          "display": "Heart rate"
-        }
-      ]
-    },
-    "valueQuantity": {
-      "value": 80,
-      "unit": "bpm"
-    },
-    
-  }
+  },
+  valueQuantity: {
+    value: 80,
+    unit: "bpm",
+  },
+};
 
-export const ObservationPatchBody ={
-    "resourceType": "Observation",
-    "id": "6469cd40-c3e0-4cb9-9a81-6cef18377fd6",
-    "status": "final",
-    "subject": {
-      "reference": "Patient/a364e9f8-dd8b-460a-900b-5b905f0a0d3d"
+export const ObservationPatchBody = {
+  resourceType: "Observation",
+  id: "6469cd40-c3e0-4cb9-9a81-6cef18377fd6",
+  status: "final",
+  subject: {
+    reference: "Patient/a364e9f8-dd8b-460a-900b-5b905f0a0d3d",
+  },
+  effectiveDateTime: "2020-01-01T00:00:00+00:00",
+  identifier: [
+    {
+      system: "my-code-system",
+      value: "ABC-12345",
     },
-    "effectiveDateTime": "2020-01-01T00:00:00+00:00",
-    "identifier": [
+  ],
+  code: {
+    coding: [
       {
-        "system": "my-code-system",
-        "value": "ABC-12345"
-      }
+        system: "http://loinc.org",
+        code: "8867-4",
+        display: "Heart rate",
+      },
     ],
-    "code": {
-      "coding": [
-        {
-          "system": "http://loinc.org",
-          "code": "8867-4",
-          "display": "Heart rate"
-        }
-      ]
+  },
+  valueQuantity: {
+    value: 123,
+    unit: "bpm",
+  },
+};
+
+export const createEncounterBody = {
+  status: "in-progress",
+  subject: {
+    reference: "Patient/dbcf46a0-2e0a-4302-9b98-18fc6a6d2fc1",
+  },
+  period : {
+    start : "2022-05-12T12:42:20.605+05:30",
+    end : "2022-03-29T23:32:26.605+05:30"
+  },
+};
+
+export const updateEncounterBody = {
+  status: "arrived",
+};
+
+export const createPatientBody = {
+  name: [
+    {
+      family: "Wick",
+      given: ["John"],
+      use: "official",
     },
-    "valueQuantity": {
-      "value": 123,
-      "unit": "bpm"
+  ],
+  birthDate : "2003-01-01",
+  gender: "male",
+};
+
+export const updatePatientBody = {
+  name: [
+    {
+      family: "Yagami",
+      given: ["Light"],
+      use: "official",
     },
-   
-  }
+  ],
+  gender: "male",
+};
+
+export const createPractitionerBody = {
+  name: [
+    {
+      family: "Uchiha",
+      given: ["Madara"],
+      use: "official",
+    },
+  ],
+  gender: "female",
+};
+
+export const updatePractitionerBody = {
+  name: [
+    {
+      family: "Smith",
+      given: ["Kevin"],
+      use: "official",
+    },
+  ],
+  gender: "male",
+};
--- routes/encounterRouter.js
+++ routes/encounterRouter.js
@@ -4,6 +4,7 @@ import {
   createEncounterResource,
   deleteEncounterResource,
   updateEncounterResource,
+  getAllObservationsOf
 } from "../controllers/encounterController.js";
 import express from "express";
 
@@ -12,7 +13,14 @@ const router = express.Router();
 router.route("/").post(createEncounterResource);
 router
   .route("/:id")
-  .get(getEncounterResource)
+  .get((req,res)=>{
+    if(req.headers.function=== "getObservations"){
+      getAllObservationsOf(req,res);
+    }
+    else{
+      getEncounterResource(req,res);
+    }
+  })
   .patch(patchEncounterResource)
   .put(updateEncounterResource)
   .delete(deleteEncounterResource);
